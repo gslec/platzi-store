e40cba886556c9ceec6ae556aaf57bfb76f31023
Ginnio Sarabia <sarabia.seven@gmail.com>
2020-06-02 10:06:17 -0500

    Construyendo un propio pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Product } from './product.model';
 })
 export class AppComponent {
   title = 'platzi-store';
+  power = 1;
   items = ['Ginnio', 'Joselyn', 'Damian'];
   products: Product[] = [
     {
