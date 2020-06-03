b9ef91a48a348d5bf269631ce57a1b68e570169a
Ginnio Sarabia <sarabia.seven@gmail.com>
2020-06-03 12:58:48 -0500

    Se arreglan arrows en Swiper

--- src/app/home/components/banner/banner.component.ts
+++ src/app/home/components/banner/banner.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent implements OnInit {
+export class BannerComponent implements OnInit, AfterViewInit {
 
   images: string[] = [
     'assets/images/banner-1.jpg',
     'assets/images/banner-2.jpg',
-    'assets/images/banner-3.jpg',
-  ]
-  constructor() { }
+    '/assets/images/banner-3.jpg',
+  ];
+  constructor() {
+
+  }
+
+  ngAfterViewInit(): void {
+    const swiper = new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
